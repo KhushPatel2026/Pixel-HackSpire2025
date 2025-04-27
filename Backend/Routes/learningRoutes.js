6d0f2fda36e77c6aa001ab5cfb81916b520ceb0a
KhushPatel2026 <khushspatel@gmail.com>
2025-04-27 07:54:29 +0530

    error solving

--- Backend/Routes/learningRoutes.js
+++ Backend/Routes/learningRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const LearningController = require('../controller/learningController');
+const LearningController = require('../Controller/learningController');
 const multer = require('multer');
 
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } });
